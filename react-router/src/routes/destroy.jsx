592b3dad74f739e4414dc90429f8bcfdbd4f41fc
chen.lili <chen.lili@jcbc.co.jp>
2024-07-11 11:49:35 +0900

    Remove all projects except

--- react-router/src/routes/destroy.jsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "react-router-dom";
-import { deleteContact } from "../contact.js";
-
-export async function action({ params }) {
-  await deleteContact(params.contactId);
-  return redirect("/");
-}
